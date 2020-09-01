eca5e5c374d36540b6e317b5511e8560de4d4d66
greenn-lab <greenn.lab@gmail.com>
2020-09-01 23:44:24 +0900

    add//dependency about servlet-api and mockito

--- apmax-agent/src/test/java/kr/co/rian/apmax/agent/test/TestSimpleASM.java
+++ apmax-agent/src/test/java/kr/co/rian/apmax/agent/test/TestSimpleASM.java
@@ -1,10 +1,27 @@
 package kr.co.rian.apmax.agent.test;
 
-public class TestSimpleASM {
+import org.mockito.Mockito;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestSimpleASM extends HttpServlet {
   
-  public static void main(String[] args) {
-    new TestSimpleASM();
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    System.out.println("HTTP request by GET method!");
+  }
+  
+  public static void main(String[] args) throws ServletException, IOException {
     System.out.println("Hello, ASM!");
+    
+    new TestSimpleASM().doGet(
+        Mockito.mock(HttpServletRequest.class),
+        Mockito.mock(HttpServletResponse.class)
+    );
   }
   
 }
